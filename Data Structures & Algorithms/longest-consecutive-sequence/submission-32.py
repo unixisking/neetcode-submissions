53c82b5d78bbad3a1f5020e117eb1d3b1c1e8ff7
Sid Ahmed <brahimi.sdhmd@gmail.com>
2026-04-23 22:31:32 +0200

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-32.py
@@ -0,0 +1,51 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        elif len(nums) == 1:
+            return 1
+
+        stack = []
+
+        for num in nums:
+            if len(stack) == 0:
+                stack.append(num)
+
+            elif num > stack[-1]:
+                stack.append(num)
+            elif num == stack[-1]:
+                continue
+            else:
+                tmp = []
+                while len(stack) > 0 and stack[-1] > num:
+                    tmp.append(stack.pop())
+                stack.append(num)
+                while len(tmp) != 0:
+                    stack.append(tmp.pop())
+
+        values = []
+
+        prevNum = stack[0]
+        count = 0
+        for i in range(1, len(stack)):
+            if stack[i] - 1 == prevNum:
+                prevNum = stack[i]
+                count += 1
+            else:
+                if prevNum == stack[i]:
+                    continue
+
+                if count > 0:
+                    values.append(count + 1)
+
+                count = 0
+                prevNum = stack[i]
+
+        if count > 0:
+            values.append(count + 1)
+
+        print("stack:", stack)
+        print("values:", values)
+        if len(values) > 0:
+            return max(values)
+        return 1
