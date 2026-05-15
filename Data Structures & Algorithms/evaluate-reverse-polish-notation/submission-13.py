f32c698c93c0e3a64c0d9d0a2c13464f3eca26c5
Sid Ahmed <brahimi.sdhmd@gmail.com>
2026-05-15 11:40:53 +0200

    Add: evaluate-reverse-polish-notation - submission-13

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-13.py
@@ -0,0 +1,59 @@
+class Solution:
+    def op(self,op1, op2, operation):
+        match operation:
+            case "+":
+                return int(op1) + int(op2)
+            case "-":
+                return int(op1) - int(op2)
+            case "*":
+                return int(op1) * int(op2)
+            case "/":
+                return int(op1) / int(op2)
+
+    def isOp(self, op):
+        if op == '*' or op == '-' or op == '/' or op == '*' or op == '+':
+            return True
+        return False
+        
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        result = 0
+        
+        i = len(tokens) - 1
+        while i >= 0:
+            stack.append(tokens[i])
+            i = i - 1
+        
+        cache = []
+        while stack:
+            result = stack.pop()
+            if not stack:
+                return int(result)
+            operand = stack.pop()
+            operator = stack.pop()
+            while not self.isOp(operator):
+                # Need to cache the result in another stack, do the operation and then put it back
+                cache.append(result)
+                result = operand
+                operand = operator
+                operator = stack.pop()
+            
+
+            result = self.op(result, operand, operator)
+            stack.append(result)
+            while cache:
+                cached = cache.pop()
+                stack.append(cached)
+
+
+        return result
+
+
+
+
+        
+
+
+
+
+        
